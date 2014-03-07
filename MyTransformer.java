30dbf7dbdbe9ec3b52a6573d17aefd07176a7d93
Liam Westby <lw322@nau.edu>
2014-03-06 17:00:23 -0700

    Revert "Create MyTransformer.java"
    This reverts commit ba641631542ddf5761dc08f7622b7b74ee66debd.

--- MyTransformer.java
+++
@@ -1,9 +0,0 @@
-public class MyTransformer implements Transformer {
-  public String getName() {
-    return "Test Name";
-  }
-  
-  public String transform(String input) {
-    return "Test string: " + input;
-  }
-}
